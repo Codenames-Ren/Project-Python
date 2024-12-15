36f8dbfbfb2890081d7b6a1c6d1e77629a470e61
Codenames-Ren <renmizuki38@gmail.com>
2024-12-15 21:29:38 +0700

    (Perubahan pada bagian function main, penambahan pilihan untuk input manual atau cepat sekaligus mengubah agar outputnya menampilkan list ketika program selesai)

--- OOP/Evanglion_OOP_Class&Obj.py
+++ OOP/Evanglion_OOP_Class&Obj.py
@@ -3,6 +3,7 @@ class Evangelion:
         self.unit = unit
         self.pilot = pilot
 
+    #Objek tampilan untuk informasi dari unit dan pilot
     def pilot_information(self):
         print (f"Unit  : {self.unit}")
         print (f"Pilot : {self.pilot}")
@@ -11,29 +12,60 @@ def main():
     list_eva = []
 
     while True:
-        print ("===== Data Pilot Eva =====")
+        print ("===== Format Input Data =====")
         print()
-        unit = input("Unit Number EVA (EX: EVA-00) : ")
-        pilot = input("Nama Pilot EVA : ")
+        print("1. Manual Input Unit dan Pilot Terpisah")
+        print("2. Input Cepat (Format Penulisan : EVA-XX, Pilot)")
         print()
-        eva = Evangelion(unit, pilot)
-        list_eva.append(eva)
+        pilihan =  input("Pilih Format Inputan [1/2] : ")
+        print()
 
-        print ("===== DATA UNIT EVA =====")
-        for eva in list_eva:
-            eva.pilot_information()
+        #Manual Input
+        if pilihan == '1':
+            unit = input("Unit Number EVA (EX: EVA-XX) : ")
+            pilot = input("Nama Pilot EVA : ")
             print()
+            list_eva.append(Evangelion(unit, pilot))
 
+        elif pilihan == '2':
+            while True:
+                eva_data = input("Masukkan Data Pilot (Format Penulisan : EVA-XX, Nama Pilot) : ")
+                print()
+                try:
+                    eva_data_entries = eva_data.split(',')
+                    unit = eva_data_entries[0].strip()
+                    pilot = eva_data_entries[1].strip()
+                    list_eva.append(Evangelion(unit, pilot))
+                    break
+                
+                except IndexError:
+                    print ("Format Penulisan Salah! Gunakan Format EVA-XX, Nama Pilot!")
+                    print()
+
+        else:
+            print("Pilihan Tidak Valid!")
+            print()
+            continue
+        
+        #Looping untuk menginput data lain atau menampilkan data yang sudah diinput
         while True:
             retry = input("Input data lain? [Y/T] : ")
             print()
             if retry.lower() == 'y':
-                print("Dimengerti.")
-                print()
                 break
+
             elif retry.lower() == 't':
-                print("Please wait...")
-                exit()
+                print ("===== Data Pilot Eva =====")
+                print()
+                if not list_eva:
+                    print("Belum ada data yang di input!")
+                else:
+                    for eva in list_eva:
+                        eva.pilot_information()
+                        print()
+                print("Checked by NERV.")
+                return
+
             else:
                 print("INVALID! Choose Y/T!")
                 print()
