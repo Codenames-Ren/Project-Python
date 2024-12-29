99a85da9bc8c013aaee9700f4a9c88ae08d2581a
Codenames-Ren <renmizuki38@gmail.com>
2024-12-30 01:05:04 +0700

    test perubahan menu.html (belum final)

--- kedai_kopi_senja/setup_database.py
+++ kedai_kopi_senja/setup_database.py
@@ -4,31 +4,39 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
 
+#hapus tabel lama kalo ada perubahan
+cursor.execute('DROP TABLE IF EXISTS menu')
+# cursor.execute('DROP TABLE IF EXISTS orders')
+# cursor.execute('DROP TABLE IF EXISTS users')
+
 #bikin tabel menu
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS menu (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    PRICE INTEGER NOT NULL
+    price INTEGER NOT NULL,
+    stock INTEGER NOT NULL,
+    image TEXT NOT NULL
 )
 ''')
 
 #bikin table orders
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS orders (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-menu_name TEXT NOT NULL,
-quantity INTEGER NOT NULL,
-total_price INTEGER NOT NULL)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    menu_name TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    total_price INTEGER NOT NULL
+)
 ''')
 
 #Bikin table users
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS users (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-username TEXT NOT NULL UNIQUE,
-password TEXT NOT NULL,
-role TEXT NOT NULL CHECK (role IN('admin', 'user'))
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL CHECK (role IN('admin', 'user'))
 )
 ''')
 
@@ -37,13 +45,13 @@ cursor.execute('DELETE FROM menu')
 
 #nambah list menunya
 cursor.executemany('''
-INSERT INTO menu (name, price) VALUES (?, ?)
+INSERT INTO menu (name, price, stock, image) VALUES (?, ?, ?, ?)
 ''', [
-    ("Espresso", 20000),
-    ("Cappuccino", 25000),
-    ("Latte", 30000),
-    ("Mocha", 35000),
-    ("Americano", 22000)
+    ("Espresso", 20000, 6, "espresso.png"),
+    ("Cappuccino", 25000, 5, "cappuccino.jpg"),
+    ("Latte", 30000, 12, "latte.jpg"),
+    ("Mocha", 35000, 20, "mocha.jpg"),
+    ("Americano", 22000, 25, "americano.png")
 ])
 
 #Hapus data user yang sebelumnya kalo ada user yang ditambah (biar gak keduplikat)
